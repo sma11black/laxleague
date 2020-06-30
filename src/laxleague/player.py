6b793a7235d2d0f213e174077407bc4ec313a1d5
sma11black <smallblack@outlook.com>
2020-07-01 04:14:04 +0800

    Jump to Error

--- src/laxleague/player.py
+++ src/laxleague/player.py
@@ -1,2 +1,14 @@
+from dataclasses import dataclass, field
+from typing import List
+
+from laxleague.guardian import Guardian
+
+
+@dataclass
 class Player:
-    pass
+    first_name: str
+    last_name: str
+    guardians: List[Guardian] = field(default_factory=list)
+
+    def add_guardians(self, guardian: Guardian):
+        self.guardians.append(guardian)
