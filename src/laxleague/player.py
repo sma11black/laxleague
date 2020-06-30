30acedf342367c33120d36fd9623ba5a2ba7b611
sma11black <smallblack@outlook.com>
2020-07-01 04:30:49 +0800

    Skipping Tests

--- src/laxleague/player.py
+++ src/laxleague/player.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import List
+from typing import List, Iterable
 
 from laxleague.guardian import Guardian
 
@@ -10,5 +10,12 @@ class Player:
     last_name: str
     guardians: List[Guardian] = field(default_factory=list)
 
-    def add_guardians(self, guardian: Guardian):
+    def add_guardian(self, guardian: Guardian):
         self.guardians.append(guardian)
+
+    def add_guardians(self, guardians: Iterable[Guardian]):
+        self.guardians.extend(guardians)
+
+    @property
+    def primary_guardian(self):
+        return self.guardians[0]
--- tests/test_player.py
+++ tests/test_player.py
@@ -1,3 +1,4 @@
+import pytest
 from laxleague.guardian import Guardian
 from laxleague.player import Player
 
@@ -9,8 +10,34 @@ def test_construction():
     assert [] == p.guardians
 
 
-def test_add_guardians():
+def test_add_guardian():
     g = Guardian('Mary', 'Jones')
     p = Player('Tatiana', 'Jones')
-    p.add_guardians(g)
+    p.add_guardian(g)
     assert [g] == p.guardians
+
+
+# @pytest.mark.skip(reason='Have not implemented method')
+def test_add_guardians():
+    p = Player('Tatiana', 'Jones')
+    # Add one guardian
+    g1 = Guardian('Mary', 'Jones')
+    p.add_guardian(g1)
+    # Later, add some more
+    g2 = Guardian('Joanie', 'Johnson')
+    g3 = Guardian('Jerry', 'Johnson')
+    p.add_guardians((g2, g3))
+    assert [g1, g2, g3] == p.guardians
+
+
+# @pytest.mark.skip()
+def test_primary_guardian():
+    p = Player('Tatiana', 'Jones')
+    # Add one guardian
+    g1 = Guardian('Mary', 'Jones')
+    p.add_guardian(g1)
+    # Later, add some more
+    g2 = Guardian('Joanie', 'Johnson')
+    g3 = Guardian('Jerry', 'Johnson')
+    p.add_guardians((g2, g3))
+    assert g1 == p.primary_guardian
